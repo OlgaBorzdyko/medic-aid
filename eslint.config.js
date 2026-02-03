46a231cae4d60273cf85448c69aa5c33506a08f0
OlgaBorzdyko <helga.borzdyko@gmail.com>
2026-02-03 18:46:26 +0400

    hot fix2

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-// eslint.config.js
 import tsPlugin from '@typescript-eslint/eslint-plugin'
 import * as tsParser from '@typescript-eslint/parser'
 import prettierPlugin from 'eslint-plugin-prettier'
@@ -8,7 +7,6 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 
 export default [
-  // Игнорируем node_modules и dist
   {
     ignores: [
       'node_modules',
@@ -19,8 +17,6 @@ export default [
       '.prettierrc.js'
     ]
   },
-
-  // Основная конфигурация для JS/TS/React
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
@@ -45,23 +41,16 @@ export default [
       'simple-import-sort': simpleImportSort
     },
     rules: {
-      // Общие JS правила
       'no-console': 'warn',
       'no-debugger': 'warn',
-
-      // React
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       'react/jsx-sort-props': [
         'warn',
         { callbacksLast: true, shorthandFirst: true }
       ],
-
-      // React Hooks
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
-
-      // TypeScript
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_' }
@@ -69,14 +58,10 @@ export default [
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/consistent-type-imports': 'warn',
       '@typescript-eslint/explicit-function-return-type': 'off',
-
-      // Импорты
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error'
     }
   },
-
-  // Prettier
   {
     files: ['**/*.{ts,tsx,js,jsx,json,css,scss,md}'],
     rules: {
