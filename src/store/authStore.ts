de3975f3cc07f9f30799899f11ed28cbd4e0a9aa
OlgaBorzdyko <helga.borzdyko@gmail.com>
2026-02-17 15:15:22 +0400

    Added authStore

---
+++ src/store/authStore.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export interface User {
+  id: number
+  name: string
+  avatar: string
+}
+
+interface AuthState {
+  user: User | null
+  token: string | null
+  login: (user: User, token: string) => void
+  logout: () => void
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+
+      login: (user, token) => {
+        set({ user, token })
+      },
+      logout: () => {
+        set({ user: null, token: null })
+      }
+    }),
+    {
+      name: 'auth-storage',
+      storage: sessionStorage,
+      partialize: (state) => ({
+        user: state.user,
+        token: state.token
+      })
+    }
+  )
+)
